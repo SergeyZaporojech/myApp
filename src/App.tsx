b284f921b52f53b1f1cff5771b45bca7b0facf9f
SergeyZaporojech <91011052+SergeyZaporojech@users.noreply.github.com>
2023-09-03 12:43:04 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+import {Routes,Route} from "react-router-dom";
+import DefaultLayout from "./components/containers/default/DefaultLayout.tsx";
+import CategoryListPage from "./components/admin/category/list/CategoryListPage.tsx";
+import CategoryCreatePage from "./components/admin/category/creat/CategoryCreatePage.tsx";
+
+function App() {
+
+  return (
+    <>
+        <Routes>
+            <Route path={"/"} element={<DefaultLayout/>}>
+                <Route index element={<CategoryListPage/>}/>
+                <Route path = {"create"} element={<CategoryCreatePage/>}/>
+            </Route>
+        </Routes>
+    </>
+  )
+}
+
+export default App
